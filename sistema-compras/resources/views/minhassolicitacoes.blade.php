770d23bfa37336106315c6e9e28d4ea179528de8
Josevaldo Silva Jr <juniorjosevaldo1@gmail.com>
2025-12-04 18:58:48 -0300

    bugfix valor unitario on my requisitions

--- sistema-compras/resources/views/minhassolicitacoes.blade.php
+++ sistema-compras/resources/views/minhassolicitacoes.blade.php
@@ -71,7 +71,7 @@
                                                 {{ $product->quantidade }}
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
-                                                R$ {{ number_format($product->price, 2, ',', '.') }}
+                                                R$ {{ number_format(($product->subtotal)/($product->quantidade), 2, ',', '.') }}
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                                 R$ {{ number_format($product->subtotal, 2, ',', '.') }}
